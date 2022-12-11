5cec554b357dec2a87b4368e239a9138eb987bfd
San Ávila <rute.leone@hotmail.com>
2022-12-11 19:14:07 -0300

    estilização do botão

--- components/DonatesAndGifts.vue
+++ components/DonatesAndGifts.vue
@@ -5,12 +5,12 @@
         </div>
         <div class="column has-text-centered is-align-self-center">
             <h1 class="title is-1 title-font title-color">Doações & Presentes</h1>
-            <p class="title is-4 text-justify text-font text-color">A Associação é o resultado da união de
+            <p class="title is-4 text-alter text-font">A Associação é o resultado da união de
                 pessoas, empresas e poder público que escolheram fazer a
                 diferença no cenário da oncologia pediátrica. Você pode ajudar a dar continuidade a esta história de
                 amor e esperança.
             </p>
-            <b-button class="button is-success">Doe aqui</b-button>
+            <b-button class="button-style"><a href="https://app.org.br/contatos/#" target="_blank">Doe aqui</a></b-button>
         </div>
     </div>
 </template>
@@ -31,12 +31,39 @@ export default {
     color: hsl(356, 75%, 45%);
 }
 
-.text-justify {
+.text-alter {
     text-align: justify;
     padding-right: 30px;
 }
 
-.text-color {
-    color: black;
+.button-style {
+    width: 200px;
+    height: 50px;
+    border: #000;
+    border-radius: 10px;
+    font-family: 'Lobster';
+
+}
+a:link{
+text-decoration:none;
+color:rgb(0, 0, 0);
+}
+
+a:visited{
+text-decoration:none;
+color:rgb(0, 0, 0);
+}
+
+a:hover{
+text-decoration:none;
+color:rgb(0, 0, 0);
+}
+
+a:active{
+text-decoration:none;
+color:rgb(0, 0, 0);
+}
+.button-style:hover {
+    transform: scale(0.88);
 }
 </style>
