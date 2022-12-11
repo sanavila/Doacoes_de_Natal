e764beb8ec9f5c7d194219fa456430136d82c0a7
San Ávila <rute.leone@hotmail.com>
2022-12-11 18:32:02 -0300

    Adicionando função de cronometragem

--- components/ChristmasCountDown.vue
+++ components/ChristmasCountDown.vue
@@ -1,22 +1,27 @@
 <template>
-    <div id="christmas-count-down" class="container-merry-christmas columns container-padding">
-        <div class="column has-text-centered is-align-self-center">
-            <h1 class="title is-1 title-font title-two-containers">O Natal está chegando!</h1>
-            <p class="title is-4 text-font color-two-containers">Que o espírito natalino viva mais do que apenas um dia no ano.</p>
-            <p>HOURS</p>
-        </div>
-        <div class="column  has-text-centered is-align-self-center">
-            <img class="image-size" src="../assets/img/imgChristmasCountDown/papaiNoelsorrindo.png"
-                alt="santa claus smiling">
-        </div>
+  <div id="christmas-count-down" class="container-merry-christmas columns container-padding">
+    <div class="column has-text-centered is-align-self-center">
+      <h1 class="title is-1 title-font title-two-containers">O Natal está chegando!</h1>
+      <p class="title is-4 text-font color-two-containers">Que o espírito natalino viva mais do que apenas um dia no
+        ano.</p>
+      <CountDown />
     </div>
+    <div class="column  has-text-centered is-align-self-center">
+      <img class="image-size" src="../assets/img/imgChristmasCountDown/papaiNoelsorrindo.png" alt="santa claus smiling">
+    </div>
+  </div>
 </template>
 
 <script>
+import CountDown from "./CountDown.vue"
 export default {
-    name: "ChristmasCountDown"
+  name: "ChristmasCountDown",
+  components: {
+    CountDown
+  }
 }
 </script>
 <style>
 @import "../assets/css/main.css";
+
 </style>
